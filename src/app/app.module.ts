0001182e3fce38200b380bd898da343dffdfd77e
mayetm93 <mayetm93@gmail.com>
2018-04-06 18:36:13 -0700

    add, delete, update tasks through db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { HttpClientModule }    from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule }     from './app-routing.module';
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -4,33 +4,105 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
- 
+
 import { Task } from './task';
-//import { TASKS } from './mock-tasks';
 import { MessageService } from './message.service';
- 
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable()
 export class TaskService {
 
-  private tasksUrl = './in-memory-data.service.ts';
-  //private tasksUrl = 'api/tasks';
+  private tasksUrl = 'api/tasks';  // URL to web api
 
   constructor(
-  	private http: HttpClient,
-  	private messageService: MessageService) { }
- 
-  getTasks(): Observable<Task[]> {
-  	//return of(TASKS);
-    return this.http.get<Task[]>(this.tasksUrl);
-  }
- 
-  getTask(id: number): Observable<Task> {
-    //this.messageService.add(`TaskService: fetched task id=${id}`);
-    //return of(TASKS.find(task => task.id === id));
-    const url = `${this.tasksUrl}/${id}`;
-  	return this.http.get<Task>(url);
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET tasks from the server */
+  getTasks (): Observable<Task[]> {
+    return this.http.get<Task[]>(this.tasksUrl)
+      .pipe(
+        tap(tasks => this.log(`fetched tasks`)),
+        catchError(this.handleError('getTasks', []))
+      );
   }
 
+  /** GET task by id. Return `undefined` when id not found */
+  getTaskNo404<Data>(id: number): Observable<Task> {
+    const url = `${this.tasksUrl}/?id=${id}`;
+    return this.http.get<Task[]>(url)
+      .pipe(
+        map(tasks => tasks[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} task id=${id}`);
+        }),
+        catchError(this.handleError<Task>(`getTask id=${id}`))
+      );
+  }
+
+  /** GET task by id. Will 404 if id not found */
+  getTask(id: number): Observable<Task> {
+    const url = `${this.tasksUrl}/${id}`;
+    return this.http.get<Task>(url).pipe(
+      tap(_ => this.log(`fetched task id=${id}`)),
+      catchError(this.handleError<Task>(`getTask id=${id}`))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new task to the server */
+  addTask (task: Task): Observable<Task> {
+    return this.http.post<Task>(this.tasksUrl, task, httpOptions).pipe(
+      tap((task: Task) => this.log(`added task w/ id=${task.id}`)),
+      catchError(this.handleError<Task>('addTask'))
+    );
+  }
+
+  /** DELETE: delete the task from the server */
+  deleteTask (task: Task | number): Observable<Task> {
+    const id = typeof task === 'number' ? task : task.id;
+    const url = `${this.tasksUrl}/${id}`;
+
+    return this.http.delete<Task>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted task id=${id}`)),
+      catchError(this.handleError<Task>('deleteTask'))
+    );
+  }
+
+  /** PUT: update the task on the server */
+  updateTask (task: Task): Observable<any> {
+    return this.http.put(this.tasksUrl, task, httpOptions).pipe(
+      tap(_ => this.log(`updated task id=${task.id}`)),
+      catchError(this.handleError<any>('updateTask'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a TaskService message with the MessageService */
   private log(message: string) {
     this.messageService.add('TaskService: ' + message);
   }
